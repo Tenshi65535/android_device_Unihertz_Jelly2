9ec95afa0d66f50d334ae0d7d1bf4458f0d0289e
Wu Junyu <wu.junyu.aur@outlook.com>
2023-03-03 10:48:08 +0900

    Fix Prebuild

--- FeliCa/Android.mk
+++ FeliCa/Android.mk
@@ -1,46 +1,51 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= FeliCaLock
 LOCAL_SRC_FILES		:= etc/FeliCaLock.apk
-LOCAL_PACKAGE_NAME	:= FeliCaLock
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= APPS
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/FeliCaLock
 LOCAL_SDK_VERSION	:= current
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= MobileFeliCaClient
 LOCAL_SRC_FILES		:= etc/MobileFeliCaClient.apk
-LOCAL_PACKAGE_NAME	:= MobileFeliCaClient
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= APPS
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaClient
 LOCAL_SDK_VERSION	:= current
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= MobileFeliCaMenuMainApp
 LOCAL_SRC_FILES		:= etc/MobileFeliCaMenuMainApp.apk
-LOCAL_PACKAGE_NAME	:= MobileFeliCaMenuMainApp
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= APPS
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaMenuMainApp
 LOCAL_SDK_VERSION	:= current
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= MobileFeliCaSettingApp
 LOCAL_SRC_FILES		:= etc/MobileFeliCaSettingApp.apk
-LOCAL_PACKAGE_NAME	:= MobileFeliCaSettingApp
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= APPS
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaSettingApp
 LOCAL_SDK_VERSION	:= current
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= MobileFeliCaWebPluginBoot
 LOCAL_SRC_FILES		:= etc/MobileFeliCaWebPluginBoot.apk
-LOCAL_PACKAGE_NAME	:= MobileFeliCaWebPluginBoot
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= APPS
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaWebPluginBoot
 LOCAL_SDK_VERSION	:= current
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
