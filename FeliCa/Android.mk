75735a9ae8fe2a1c303405893240f1c581003f26
Wu Junyu <wu.junyu.aur@outlook.com>
2023-03-03 09:32:39 +0900

    Fix LOCAL_SDK_VERSION

--- FeliCa/Android.mk
+++ FeliCa/Android.mk
@@ -5,6 +5,7 @@ LOCAL_PACKAGE_NAME	:= FeliCaLock
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/FeliCaLock
+LOCAL_SDK_VERSION	:= current
 include $(BUILD_PACKAGE)
 
 LOCAL_PATH := $(call my-dir)
@@ -14,6 +15,7 @@ LOCAL_PACKAGE_NAME	:= MobileFeliCaClient
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaClient
+LOCAL_SDK_VERSION	:= current
 include $(BUILD_PACKAGE)
 
 LOCAL_PATH := $(call my-dir)
@@ -23,6 +25,7 @@ LOCAL_PACKAGE_NAME	:= MobileFeliCaMenuMainApp
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaMenuMainApp
+LOCAL_SDK_VERSION	:= current
 include $(BUILD_PACKAGE)
 
 LOCAL_PATH := $(call my-dir)
@@ -32,6 +35,7 @@ LOCAL_PACKAGE_NAME	:= MobileFeliCaSettingApp
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaSettingApp
+LOCAL_SDK_VERSION	:= current
 include $(BUILD_PACKAGE)
 
 LOCAL_PATH := $(call my-dir)
@@ -41,4 +45,5 @@ LOCAL_PACKAGE_NAME	:= MobileFeliCaWebPluginBoot
 LOCAL_CERTIFICATE 	:= PRESIGNED
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_APPS)/MobileFeliCaWebPluginBoot
+LOCAL_SDK_VERSION	:= current
 include $(BUILD_PACKAGE)
